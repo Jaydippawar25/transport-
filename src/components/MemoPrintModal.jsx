d9915aeb39ce9649080bf241b314354877523031
Jaydippawar25 <jaydippawar2512@gmail.com>
2026-09-25 09:35:08 +0530

    Align TOTAL labels to the right in Memo print footer

--- src/components/MemoPrintModal.jsx
+++ src/components/MemoPrintModal.jsx
@@ -128,7 +128,7 @@ export default function MemoPrintModal({ memo, onClose }) {
                   <td className="py-1.5 px-1 text-right text-[11px]">{memo.totalTbb > 0 ? memo.totalTbb.toLocaleString('en-IN') : ''}</td>
                 </tr>
                 <tr className="font-bold border-t-2 border-black bg-slate-100">
-                  <td colSpan={7} className="border-r border-black py-1.5 px-2 text-center">TOTAL L.R. AMOUNT</td>
+                  <td colSpan={7} className="border-r border-black py-1.5 px-2 text-right">TOTAL L.R. AMOUNT</td>
                   <td colSpan={3} className="py-1.5 px-1 text-center">
                     { (Number(memo.totalToPay || 0) + Number(memo.totalPaid || 0) + Number(memo.totalTbb || 0)) > 0 
                         ? (Number(memo.totalToPay || 0) + Number(memo.totalPaid || 0) + Number(memo.totalTbb || 0)).toLocaleString('en-IN') 
@@ -136,19 +136,19 @@ export default function MemoPrintModal({ memo, onClose }) {
                   </td>
                 </tr>
                 <tr className="font-bold border-t-2 border-black bg-slate-100">
-                  <td colSpan={7} className="border-r border-black py-1.5 px-2 text-center">FREIGHT</td>
+                  <td colSpan={7} className="border-r border-black py-1.5 px-2 text-right">FREIGHT</td>
                   <td colSpan={3} className="py-1.5 px-1 text-center">{memo.freight ? Number(memo.freight).toLocaleString('en-IN') : ''}</td>
                 </tr>
                 <tr className="font-bold border-t-2 border-black bg-slate-100">
-                  <td colSpan={7} className="border-r border-black py-1.5 px-2 text-center">LOADING CHARGES</td>
+                  <td colSpan={7} className="border-r border-black py-1.5 px-2 text-right">LOADING CHARGES</td>
                   <td colSpan={3} className="py-1.5 px-1 text-center">{memo.loadingCharges ? Number(memo.loadingCharges).toLocaleString('en-IN') : ''}</td>
                 </tr>
                 <tr className="font-bold border-t-2 border-black bg-slate-100">
-                  <td colSpan={7} className="border-r border-black py-1.5 px-2 text-center">OTHER CHARGES</td>
+                  <td colSpan={7} className="border-r border-black py-1.5 px-2 text-right">OTHER CHARGES</td>
                   <td colSpan={3} className="py-1.5 px-1 text-center">{memo.otherCharges ? Number(memo.otherCharges).toLocaleString('en-IN') : ''}</td>
                 </tr>
                 <tr className="font-bold border-t-2 border-black bg-slate-100">
-                  <td colSpan={7} className="border-r border-black py-1.5 px-2 text-center">G. TOTAL</td>
+                  <td colSpan={7} className="border-r border-black py-1.5 px-2 text-right">G. TOTAL</td>
                   <td colSpan={3} className="py-1.5 px-1 text-center">
                     { (Number(memo.freight || 0) + Number(memo.loadingCharges || 0) + Number(memo.otherCharges || 0)) > 0 
                       ? (Number(memo.freight || 0) + Number(memo.loadingCharges || 0) + Number(memo.otherCharges || 0)).toLocaleString('en-IN') 
